381e4db436e1133aeec58d3468ed25dfb6b18a85
234968 <234968@KB37-309-C09>
2024-09-11 10:59:50 +0300

    Добавьте файлы проекта.

---
+++ Projectt1.cpp/nnn.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int integ = 25;
+int teg;
+int c12;
+
+int main()
+{
+	teg = 85;
+	cout << "sdsfsf";
+	wchar_t i1{ L'i' };
+	std::wcout << i1 << '\n';
+
+	c12 = 25;
+	char w11[] = "west";
+	teg = 55;
+	string w1 = "Lady";
+	int int2 = 20;
+	string s1 = "hello";
+	char ch1 = 's';
+	double d1 = 25;
+	float f1 = 25.6;
+	int a1, b1, c1;
+	char tt5 = 'A';
+	int kk89 = 25;
+	char bb7 = 'H';
+	string str = "Hello";
+	char b000 = 'B';
+	int a2, a3;
+	int c3 = 18;
+	s1 = "hi";
+	char t1[] = "lets go";
+	int inerg = 22; 
+	cout << integ << endl<< inerg <<endl;
+	return 0;
+}
